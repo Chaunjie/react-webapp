2958e4add35e7de0bd56f4e701abb6847e04925e
xudao <894576801@qq.com>
2016-06-21 17:50:25 +0800

    #0:add react-router

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/Main.css');
 
 import React from 'react';
+import {Link} from 'react-router';
 
 class AppComponent extends React.Component {
   render() {
@@ -14,7 +15,7 @@ class AppComponent extends React.Component {
               <span className="aui-badge aui-badge-danger aui-pull-right">V 1.2.0</span>
             </div>
           </li>
-          <li className="aui-list-view-cell" tapmode onclick="share()" id="share">
+          <li className="aui-list-view-cell"  onclick="share()" id="share">
             <div className="aui-arrow-right aui-text-primary">
                 <span>分享至朋友圈</span>
             </div>
@@ -23,125 +24,125 @@ class AppComponent extends React.Component {
 
         <div className="aui-content">
           <ul className="aui-list-view">
-            <li className="aui-list-view-cell" tapmode onclick="openWin('pull_refresh')">
+            <li className="aui-list-view-cell"  onclick="openWin('pull_refresh')">
               <a href="#SearchBar" className="aui-arrow-right">下拉刷新组件（一）<span className="aui-badge aui-badge-danger">新增</span></a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('pull_refresh_2')">
-              <a className="aui-arrow-right">下拉刷新组件（二）<span className="aui-badge aui-badge-danger">新增</span></a>
+            <li className="aui-list-view-cell"  onclick="openWin('pull_refresh_2')">
+             <Link to="/SearchBar" className="aui-arrow-right">下拉刷新组件（二）<span className="aui-badge aui-badge-danger">新增</span></Link>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('slide')">
+            <li className="aui-list-view-cell"  onclick="openWin('slide')">
               <a className="aui-arrow-right">轮播组件<span className="aui-badge aui-badge-danger">新增</span></a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_swipe')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_swipe')">
               <a className="aui-arrow-right">列表右滑显示菜单<span className="aui-badge aui-badge-danger">新增</span></a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('accordion')">
+            <li className="aui-list-view-cell"  onclick="openWin('accordion')">
               <a className="aui-arrow-right">手风琴折叠面板<span className="aui-badge aui-badge-danger">新增</span></a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('flex')">
+            <li className="aui-list-view-cell"  onclick="openWin('flex')">
               <a className="aui-arrow-right">AUI Flex<span className="aui-badge aui-badge-warning">优化</span></a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('border')">
+            <li className="aui-list-view-cell"  onclick="openWin('border')">
               <a className="aui-arrow-right">边框样式</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('indexed_list')">
+            <li className="aui-list-view-cell"  onclick="openWin('indexed_list')">
               <a className="aui-arrow-right">索引列表<span className="aui-badge aui-badge-success">优化</span></a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openSlider()">
+            <li className="aui-list-view-cell"  onclick="openSlider()">
               <a className="aui-arrow-right">上下滑动全屏展示</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('calendar')">
+            <li className="aui-list-view-cell"  onclick="openWin('calendar')">
               <a className="aui-arrow-right">日历</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('calendar_slide')">
+            <li className="aui-list-view-cell"  onclick="openWin('calendar_slide')">
               <a className="aui-arrow-right">日历上下滑动展示</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('button')">
+            <li className="aui-list-view-cell"  onclick="openWin('button')">
               <a className="aui-arrow-right">按钮</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('form')">
+            <li className="aui-list-view-cell"  onclick="openWin('form')">
               <a className="aui-arrow-right">表单</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('switch')">
+            <li className="aui-list-view-cell"  onclick="openWin('switch')">
               <a className="aui-arrow-right">switch开关</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('checkbox')">
+            <li className="aui-list-view-cell"  onclick="openWin('checkbox')">
               <a className="aui-arrow-right">checkbox选择</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('radio')">
+            <li className="aui-list-view-cell"  onclick="openWin('radio')">
               <a className="aui-arrow-right">radio选择</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list')">
+            <li className="aui-list-view-cell"  onclick="openWin('list')">
               <a className="aui-arrow-right">普通列表</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_arrow')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_arrow')">
               <a className="aui-arrow-right">带有角标和箭头的列表</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_thumb')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_thumb')">
               <a className="aui-arrow-right">缩略图列表</a>
             </li>
 
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_image')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_image')">
               <a className="aui-arrow-right">图片列表布局</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('header_bar')">
+            <li className="aui-list-view-cell"  onclick="openWin('header_bar')">
               <a className="aui-arrow-right">导航栏</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('footer_tab')">
+            <li className="aui-list-view-cell"  onclick="openWin('footer_tab')">
               <a className="aui-arrow-right">底部工具栏</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_nine')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_nine')">
               <a className="aui-arrow-right">九宫格</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_sixteen')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_sixteen')">
               <a className="aui-arrow-right">十六宫格</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('loading')">
+            <li className="aui-list-view-cell"  onclick="openWin('loading')">
               <a className="aui-arrow-right">个性loading</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('tab')">
+            <li className="aui-list-view-cell"  onclick="openWin('tab')">
               <a className="aui-arrow-right">TAB菜单</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('contact_book')">
+            <li className="aui-list-view-cell"  onclick="openWin('contact_book')">
               <a className="aui-arrow-right">通讯录</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('chat')">
+            <li className="aui-list-view-cell"  onclick="openWin('chat')">
               <a className="aui-arrow-right">聊天界面</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('iconfont')">
+            <li className="aui-list-view-cell"  onclick="openWin('iconfont')">
               <a className="aui-arrow-right">图标</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('list_counter')">
+            <li className="aui-list-view-cell"  onclick="openWin('list_counter')">
               <a className="aui-arrow-right">计数列表(购物车类样式疯狂拼合)</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('dialog')">
+            <li className="aui-list-view-cell"  onclick="openWin('dialog')">
               <a className="aui-arrow-right">弹出提示样式(dialog)</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('status')">
+            <li className="aui-list-view-cell"  onclick="openWin('status')">
               <a className="aui-arrow-right">带状态的表单／列表</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('toast')">
+            <li className="aui-list-view-cell"  onclick="openWin('toast')">
               <a className="aui-arrow-right">Toast</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('tips')">
+            <li className="aui-list-view-cell"  onclick="openWin('tips')">
               <a className="aui-arrow-right">信息提示条</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('searchbar')">
+            <li className="aui-list-view-cell"  onclick="openWin('searchbar')">
               <a className="aui-arrow-right">搜索条</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('range')">
+            <li className="aui-list-view-cell"  onclick="openWin('range')">
               <a className="aui-arrow-right">滑块</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('timeline')">
+            <li className="aui-list-view-cell"  onclick="openWin('timeline')">
               <a className="aui-arrow-right">时间轴</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('progress')">
+            <li className="aui-list-view-cell"  onclick="openWin('progress')">
               <a className="aui-arrow-right">进度条</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('alert')">
+            <li className="aui-list-view-cell"  onclick="openWin('alert')">
               <a className="aui-arrow-right">自定义弹出层</a>
             </li>
-            <li className="aui-list-view-cell" tapmode onclick="openWin('waterfall')">
+            <li className="aui-list-view-cell"  onclick="openWin('waterfall')">
               <a className="aui-arrow-right">瀑布流</a>
             </li>
           </ul>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+/*import ReactCSSTransitionGroup from 'react-addons-css-transition-group';*/
 import { Router, Route, IndexRoute} from 'react-router';
 import Main from './components/Main';
 import SearchBar from './components/Searchbar';
@@ -9,17 +9,9 @@ import SearchBar from './components/Searchbar';
 class TestApp extends React.Component {
   render() {
     return (
-      <ReactCSSTransitionGroup
-        component="div"
-        transitionName="page"
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}
-        style={{height: '100%'}}
-      >
-        {React.cloneElement(this.props.children, {
-          key: this.props.location.pathname
-        })}
-      </ReactCSSTransitionGroup>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
@@ -29,7 +21,7 @@ ReactDOM.render((
   <Router>
     <Route path="/" component={TestApp}>
       <IndexRoute component={Main}/>
-      <Route path="SearchBar" component={SearchBar}/>
+      <Route path="/SearchBar" component={SearchBar}/>
     </Route>
   </Router>
 ), document.getElementById('app'));
